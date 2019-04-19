909472bc51b1ac267063d037a438a7dbc01efd24
ljw26 <ljw26@st-andrews.ac.uk>
2019-04-19 14:56:20 +0100

    added logging

--- src/server/ServerNode.java
+++ src/server/ServerNode.java
@@ -8,7 +8,13 @@ import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class ServerNode {
     private int id;
@@ -23,6 +29,8 @@ public class ServerNode {
     private ArrayList<Node> nodes = new ArrayList<>();
     private Socket serverToTalkTo;
     private Node nextNode = null;
+    private String loggerFileName;
+    private PrintWriter logger;
 
     public ServerNode(int id, String host, int port, int coordinatorId, String coordinatorHost, int coordinatorPort) throws IOException {
         this.id = id;
@@ -31,18 +39,31 @@ public class ServerNode {
         this.coordinatorId = coordinatorId;
         this.coordinatorHost = coordinatorHost;
         this.coordinatorPort = coordinatorPort;
+        this.loggerFileName = "Server" + id + "Log.log";
+
+        logger = new PrintWriter(new FileWriter(loggerFileName), true);
+        logger.println(getTimestamp() + "New server created.");
     }
 
     private boolean isCoordinator() {return isCoordinator;}
 
+    private String getTimestamp() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return "[" + dateFormat.format(date) + "] ";
+    }
+
     private void initialiseServer() throws IOException {
         listener = new ServerSocket(port);
+        logger.println(getTimestamp() + "Server " + id + " now listening.");
         if (id == coordinatorId) {
+            logger.println(getTimestamp() + "Server is coordinator.");
             isCoordinator = true;
         }
     }
 
     private void buildNodeList() {
+        logger.println(getTimestamp() + "Reading host file.");
         try {
             FileReader fr = new FileReader(HOST_FILE);
             CSVReader csvReader = new CSVReaderBuilder(fr).withSkipLines(1).build();
@@ -56,21 +77,26 @@ public class ServerNode {
                 nodes.add(new Node(id, host, port));
             }
         } catch (FileNotFoundException e) {
+            logger.println(getTimestamp() + "ERROR could not find file.");
             e.printStackTrace();
         } catch (IOException e) {
+            logger.println(getTimestamp() + "ERROR IO Exception.");
             e.printStackTrace();
         }
     }
 
     private void printNodeList() {
+        logger.println(getTimestamp() + "Host file read.");
         for (Node node : nodes) {
             System.out.println(node.getId() + ", " + node.getHost() + ":" + node.getPort());
         }
     }
 
     private boolean checkAllServersOnline() {
+        logger.println(getTimestamp() + "Checking network state.");
         for (Node node : nodes) {
             try {
+                logger.println(getTimestamp() + "Checking Server " + node.getId());
                 serverToTalkTo = new Socket(node.getHost(), node.getPort());
                 BufferedReader inFromServer = new BufferedReader(new InputStreamReader(serverToTalkTo.getInputStream()));
                 PrintWriter outToServer = new PrintWriter(new OutputStreamWriter(serverToTalkTo.getOutputStream()), true);
@@ -89,12 +115,15 @@ public class ServerNode {
 
                 serverToTalkTo.close();
             } catch (UnknownHostException e) {
+                logger.println(getTimestamp() + "ERROR unknown host.");
                 e.printStackTrace();
                 return false;
             } catch (ConnectException e) {
+                logger.println(getTimestamp() + "ERROR Server " + node.getId() + " is not communicating.");
                 System.out.println(node.getId() + " is not online.");
                 return false;
             } catch (IOException e) {
+                logger.println(getTimestamp() + "ERROR IO Exception.");
                 e.printStackTrace();
                 return false;
             }
@@ -104,13 +133,16 @@ public class ServerNode {
     }
 
     private void setNextNode(Node nextNode) {
+        logger.println(getTimestamp() + "Setting next node to " + nextNode.getId());
         this.nextNode = nextNode;
     }
 
     private void buildRing() {
+        logger.println(getTimestamp() + "Beginning ring construction.");
         for (int i = 0; i < nodes.size(); i++) {
             Node currentNode = nodes.get(i);
             try {
+                logger.println(getTimestamp() + "Updating Server " + currentNode.getId() + " next node.");
                 serverToTalkTo = new Socket(currentNode.getHost(), currentNode.getPort());
                 BufferedReader inFromServer = new BufferedReader(new InputStreamReader(serverToTalkTo.getInputStream()));
                 PrintWriter outToServer = new PrintWriter(new OutputStreamWriter(serverToTalkTo.getOutputStream()), true);
@@ -129,15 +161,20 @@ public class ServerNode {
                     }
 
                     outToServer.println(nextNode.getId() + "," + nextNode.getHost() + "," + nextNode.getPort());
+                    logger.println(getTimestamp() + "New next node sent.");
                 }
 
+                logger.println(getTimestamp() + "Closing connection.");
                 serverToTalkTo.close();
 
             } catch (UnknownHostException e) {
+                logger.println(getTimestamp() + "ERROR unknown host.");
                 e.printStackTrace();
             } catch (ConnectException e) {
-                System.out.println(currentNode.getId() + " has stopped communicating.");
+                logger.println(getTimestamp() + "ERROR Server " + currentNode.getId() + " is not communicating.");
+                System.out.println(currentNode.getId() + " is not online.");
             } catch (IOException e) {
+                logger.println(getTimestamp() + "ERROR IO Exception.");
                 e.printStackTrace();
             }
         }
@@ -151,8 +188,10 @@ public class ServerNode {
 
     private void listenForConnections() throws IOException {
         while (true) {
+            logger.println(getTimestamp() + "Listening for connection...");
             System.out.println("Listening...");
             Socket connected = listener.accept();
+            logger.println(getTimestamp() + "Client " + connected.toString() + " connected.");
             System.out.println("Speaking to " + connected.toString());
 
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connected.getInputStream()));
@@ -160,18 +199,26 @@ public class ServerNode {
 
             String msg = inFromClient.readLine();
             System.out.println(msg);
+            logger.println(getTimestamp() + "Received " + msg);
 
             if (msg.equals("HELLO")) {
+                logger.println(getTimestamp() + "Acknowledging client.");
                 outToClient.println("OK");
 
                 msg = inFromClient.readLine();
+                logger.println(getTimestamp() + "Received " + msg);
                 System.out.println(msg);
 
-                outToClient.println("Hello Coordinator! From " + id);
+                String msgToSend = "Hello Coordinator! From " + id;
+                logger.println(getTimestamp() + "Sending " + msgToSend);
+                outToClient.println(msgToSend);
+
             } else if (msg.equals("NEXT NODE")) {
+                logger.println(getTimestamp() + "Acknowledging client.");
                 outToClient.println("OK");
 
                 msg = inFromClient.readLine();
+                logger.println(getTimestamp() + "Received new next node.");
                 String[] splitMessage = msg.split(",");
 
                 int nextId = Integer.parseInt(splitMessage[0]);
@@ -182,6 +229,7 @@ public class ServerNode {
                 printNextNode();
             }
 
+            logger.println(getTimestamp() + "Closing connection.");
             connected.close();
         }
     }
